f747a60f9b8925d48c18a82481a8ab61b1c5f39a
Kevin Schiebelbein <kschiebelbein@lartirigoyen.com>
2023-11-24 14:30:33 -0300

    Servicio base para Appgro

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,6 @@
-FROM python:3.12.0-slim
+FROM python:3.12.0-alpine3.18
 COPY ./app /app
 WORKDIR /app
-
-#ARG POETRY_VERSION=1.7.0
-RUN python3 -m pip install --no-cache-dir --upgrade -r /app/requirements.txt
-
-# Instalo Poetry
-#RUN curl -sSL https://install.python-poetry.org | POETRY_HOME=/usr/local/poetry python3 - --version ${POETRY_VERSION}
-#ENV POETRY_HOME="/usr/local/poetry/bin"
-#ENV PATH="$PATH:$POETRY_HOME"
-
-#CMD ["python3", "-m", "poetry", "--version"]
-#CMD ["python3", "--version"]
-#RUN #!/bin/sh \
-    #if [ -f "./app/pyproject.toml" ]; then \ 
-      #poetry install \ 
-    #else \
-      #echo "Poetry no se inicio en este proyecto" \
-    #fi
-
+#RUN pip3 install --use-pep517 -r /app/requirements.txt
+RUN pip3 install -r /app/requirements.txt
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80", "--reload"]
--- app/routers/router.py
+++ app/routers/router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from dotenv import dotenv_values
+from pydantic import BaseModel
+import requests
 #from dependencies import softcereal_connection
 
 config = dotenv_values(".env")
@@ -9,12 +11,22 @@ router = APIRouter(
   #tags=["Endpoints disponibles"]
 )
 
+# Defino modelo para los requests body
+class DepositoArticulo(BaseModel):
+  deposito : int = 0
+  articulo : int = 0
+  partidas : bool = False
+
 @router.get("/", tags=["Root"])
 async def index():
   return {"msg": f"Hello {config['APP_TITLE']}"}
 
-@router.get("/example", status_code=200, tags=["Example"], name="Example", summary="Example description")
-async def example(param: str = 0):
+@router.post("/deposito_articulo", status_code=200, tags=["Example"], name="Example", summary="Example description")
+async def deposito_articulo(data_body: DepositoArticulo):
+  deposito = data_body.deposito
+  articulo = data_body.articulo
+  partidas = data_body.partidas
+  
   data = None
   return {
     "msg" : "Example",
